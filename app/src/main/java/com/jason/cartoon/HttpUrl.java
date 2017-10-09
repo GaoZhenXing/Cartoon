49813db787f54196276fb3fcf9e598691581c068
Jason <zx.gao@foxmail.com>
2017-10-09 09:24:10 +0800

    2017年10月9日 早上提交

---
+++ app/src/main/java/com/jason/cartoon/HttpUrl.java
@@ -0,0 +1,25 @@
+package com.jason.cartoon;
+
+public class HttpUrl {
+
+    /**************************************************/
+
+    public static final String APP_HTTP = "http://www.ishuhui.net";
+    public static final String GET_ALL_BOOK = "http://www.ishuhui.net/ComicBooks/GetSubscribe";
+    public static final String GET_NEW_BOOK = "http://www.ishuhui.net/ComicBooks/GetLastChapterForBookIds?idJson=[1,2,3]";
+    public static final String URL_CATEGORY_DATA = "http://www.ishuhui.net/ComicBooks/GetAllBook";
+    public static final String URL_COMIC_BOOK_DATA = "http://www.ishuhui.net/ComicBooks/GetChapterList";
+    public static final String URL_IMG_CHAPTER = "http://www.ishuhui.net/ReadComicBooksToIso/";
+    public static final String URL_RECOMMEND = "http://www.ishuhui.net/ComicBooks/GetAllBook";
+    public static final String URL_SEARCH_DATA = "http://www.ishuhui.net/ComicBooks/GetAllBook";
+    public static final String URL_SLIDE_DATA = "http://two.ishuhui.com/imgs.html";
+    public static final String URL_SUBCRIBE_USER = "http://www.ishuhui.net/ComicBooks/GetAllBook";
+    public static final String URL_USER_LOGIN = "http://www.ishuhui.net/UserCenter/Login";
+    public static final String URL_USER_REGISTER = "http://www.ishuhui.net/UserCenter/Regedit";
+    public static final String URL_USER_SUBSCRIBE = "http://www.ishuhui.net/Subscribe";
+    public static final String URL_WEEK_SEVEN = "http://www.ishuhui.net/ComicBooks/GetAllBook";
+
+
+    /**************************************************/
+
+}
---
+++ app/src/main/java/com/jason/cartoon/base/XActivity.java
@@ -0,0 +1,15 @@
+package com.jason.cartoon.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 项目名称：Cartoon
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ * 创建人：JasonGao
+ * 创建日期：2017/9/30.9:58
+ */
+
+public class XActivity extends AppCompatActivity {
+    
+}
